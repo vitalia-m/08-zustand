9935287352ba80934b67100de036fa8192dd2de9
Vitalia <vitaliya.m05@gmail.com>
2025-07-11 17:42:19 +0200

    Fixing vercel errors

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -35,7 +35,7 @@ export async function generateMetadata({
 }
 
 export default async function Notes({ params }: NotesProps) {
-  const { slug } = await params;
+  const slug = params.slug;
   const tag = slug[0] === "All" ? undefined : slug[0];
   const response = await fetchNotes("", 1, tag);
 
